215c9d1a44f4e1dbde8279ea853335693a055b7a
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2024-03-26 02:28:44 +0530

    Some Changes

--- src/main/java/com/exploremore/controller/InformationController.java
+++ src/main/java/com/exploremore/controller/InformationController.java
@@ -2,15 +2,19 @@ package com.exploremore.controller;
 
 import com.exploremore.dao.HotelRepository;
 import com.exploremore.dao.InformationRepository;
+import com.exploremore.dao.PackageRepository;
 import com.exploremore.entites.Hotel;
 import com.exploremore.entites.Package;
 import com.exploremore.entites.PackageInformation;
+import com.exploremore.exception.NoElementFoundException;
+import org.hibernate.annotations.ParamDef;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/details")
@@ -22,6 +26,9 @@ public class InformationController {
     @Autowired
     private HotelRepository hotelRepo;
 
+    @Autowired
+    private PackageRepository pacRepo;
+
     @GetMapping("/all")
     @PreAuthorize("hasAnyRole('MANAGER','USER','ADMIN')")
     public List<PackageInformation> all()
@@ -29,10 +36,25 @@ public class InformationController {
         return infoRepo.findAll();
     }
 
+    @GetMapping("/{id}")
+    public PackageInformation getByID(@PathVariable Long id)
+    {
+        if(infoRepo.findById(id).isEmpty())
+        {
+            throw new NoElementFoundException("No Such Package Found");
+        }
+        Optional<PackageInformation> optionalUser = infoRepo.findById(id);
+        return optionalUser.orElse(null);
+    }
+
     @GetMapping("/package/{id}")
     @PreAuthorize("hasAnyRole('MANAGER','USER','ADMIN')")
     public List<PackageInformation> package_by_id(@PathVariable long id)
     {
+        if(pacRepo.findById((int) id).isEmpty())
+        {
+            throw new NoElementFoundException("No Such Package Found");
+        }
         List<PackageInformation> list = infoRepo.findAll();
         List<PackageInformation> packages = new ArrayList<>();
         for(PackageInformation info : list) {
@@ -46,6 +68,10 @@ public class InformationController {
     @GetMapping("/package/{id}/hotels")
     @PreAuthorize("hasAnyRole('MANAGER','USER','ADMIN')")
     public List<Hotel> hotelsByPackageId(@PathVariable long id) {
+        if(pacRepo.findById((int) id).isEmpty())
+        {
+            throw new NoElementFoundException("No Such Package Found");
+        }
         List<PackageInformation> list = infoRepo.findAll();
         List<Hotel> hotels = new ArrayList<>();
         for(PackageInformation info : list) {
@@ -59,6 +85,10 @@ public class InformationController {
     @GetMapping("/hotel/{id}/packages")
     @PreAuthorize("hasAnyRole('MANAGER','USER','ADMIN')")
     public List<Package> packagesByHotelId(@PathVariable long id) {
+        if(hotelRepo.findById((int) id).isEmpty())
+        {
+            throw new NoElementFoundException("No Such Hotel Found");
+        }
         List<PackageInformation> list = infoRepo.findAll();
         List<Package> packages = new ArrayList<>();
         for(PackageInformation info : list) {
@@ -68,4 +98,34 @@ public class InformationController {
         }
         return packages;
     }
+
+    @PostMapping("/add")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
+    public void add(@RequestBody PackageInformation info)
+    {
+        infoRepo.save(info);
+    }
+
+    @PutMapping("/update/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
+    public void update(@PathVariable long id, @RequestBody PackageInformation info)
+    {
+        if(infoRepo.findById(id).isEmpty())
+        {
+            throw new NoElementFoundException("No Such Package Found");
+        }
+        infoRepo.save(info);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
+    public void delete(@PathVariable long id)
+    {
+        if(infoRepo.findById(id).isEmpty())
+        {
+            throw new NoElementFoundException("No Such Package Found");
+        }
+        infoRepo.deleteById(id)
+    }
+
 }
